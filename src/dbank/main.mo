58e460effd892c779347e281e1ce346e33bcbdbe
simoneya <yavar.simona@yahoo.com>
2022-06-21 10:29:24 +0100

    first-DApp

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,53 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float"; //converts to floating number.
+
+actor DBank {
+  stable var currentValue: Float = 300;
+  currentValue := 300;
+  Debug.print(debug_show(currentValue));
+
+
+   stable var startTime = Time.now();
+   startTime := Time.now();
+   Debug.print(debug_show(startTime)); //To print time in nanoseconds since  1970-01-01.
+
+  let id = 4756487628448654756;
+   //Debug.print(debug_show(id));
+
+
+  //Top Up function
+  public func topUp(amount: Float) {     //provide data type for amount input which is a natural number. Natural number(Nat) any number which is positive.
+   currentValue += amount;
+   Debug.print(debug_show(currentValue));
   };
-};
+
+
+
+  //Withdrawl function
+  public func withdraw(amount: Float) {
+    let tempValue: Float = currentValue - amount;
+    if(tempValue - amount >= 0) {
+    currentValue -= amount;
+    Debug.print(debug_show(currentValue));
+    } else {
+      Debug.print("Amount too large, currentValue less than zero.")
+    }
+  };
+
+
+  public query func checkBalance(): async Float {
+    return currentValue; //read only operation. We are not modifying anything.That's why we use query method.
+  };
+
+
+
+  public func compound () {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS)); //float makes both sides with same datatype.
+    startTime := currentTime;
+  };
+
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,44 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+//to update balance on Current Balance field everytime dapp gets loaded.
+window.addEventListener("load", async function() {
+    //console.log("Finish loading.");
+    update();
 });
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+    event.preventDefault();//To prevent form from default refresh everytime you submit.
+    //console.log("Submitted.");
+
+    const button = event.target.querySelector("#submit-btn");
+
+    //Get the value from input and output inputs.
+    const inputAmount = parseFloat(document.getElementById("input-amount").value); //change default datatype(Int) to Float.
+    const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value); //same.
+
+    button.setAttribute("disabled", true); //once clicked on submit button it will become disable.
+
+    if (document.getElementById("input-amount").value.length != 0) {
+        await dbank.topUp(inputAmount);
+    }
+
+    if(document.getElementById("withdrawal-amount").value.length != 0) {
+        await dbank.withdraw(outputAmount);
+    }
+ 
+
+    await dbank.compound();
+
+    update()
+
+    document.getElementById("input-amount").value = "" ;
+    document.getElementById("withdrawal-amount").value = "" ;
+    button.removeAttribute("disabled"); //to make button enable again.
+});
+
+async function update() {
+     //To display value after topup your account.
+     const currentAmount = await dbank.checkBalance();
+     document.getElementById("value").innerText = Math.round(currentAmount *100) / 100;
+};
+
